6a370418f787652c16cafe3cde331f601ff5660d
KOMALSHARMA <komalsharmagcloud@gmail.com>
2026-03-09 00:56:59 +0530

    Implement recursive function to print numbers from n to 1
    Applied the concept from the previous problem. 
    To print numbers from N to 1 (reverse order), the print statement is placed before the recursive call so the current value prints first before moving deeper into recursion.

--- Recursion/Print_n_to_1.cpp
+++ Recursion/Print_n_to_1.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+void Print_N_to_1(int n){
+    if(n==0){
+        return;
+    }
+    else{
+        cout<<n<<" ";
+        return Print_N_to_1(n-1);
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    Print_N_to_1(n);
+
+    return 0;
+}
