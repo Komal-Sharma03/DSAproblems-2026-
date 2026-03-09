e19f904e20ef07350166ad750f2c9e0970b4acde
KOMALSHARMA <komalsharmagcloud@gmail.com>
2026-03-09 23:32:04 +0530

    Add SecondLargest function to find second largest element
    Problem:
    Find the second largest element in an array.
    
    Approach:
    Traverse the array one time while keeping track of the largest and second largest elements.
    
    Time Complexity:
    O(n)
    
    Space Complexity:
    O(1)
    
    Learning:
    Careful initialization and handling edge cases (like negative numbers) is important when designing algorithms.

--- Array/second_largest_element.cpp
+++ Array/second_largest_element.cpp
@@ -1,1 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int SecondLargest(vector<int> &arr){
+      int n=arr.size();
+      int ans1=-1;
+      int ans2=-1;
+      for(int i=0;i<n;i++){
+          if(arr[i]>ans1){
+            ans2=ans1;
+            ans1=arr[i];
+          }
+          else if(arr[i]<ans1 && arr[i]>ans2){
+            ans2=arr[i];
+          }
+      }
+      if(ans2 == INT_MIN){
+        return -1;
+      }
+      return ans2;
+}
+
+int main(){
+    vector<int> arr={13,0,-1,-6};
+    cout<<SecondLargest(arr);
+
+    return 0;
+}
