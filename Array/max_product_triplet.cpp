9f07733af813d544d41a4d7dcf8d4a3673ae5930
KOMALSHARMA <komalsharmagcloud@gmail.com>
2026-03-10 23:51:13 +0530

    Add MaxProduct function to calculate max triplet product
    Problem : Given an integer array, find a maximum product of a triplet in the array.
    
    Approach : I came accross 3 approaches to solve this problem.
    1. Checking every pair of three elemnts to  find the maximum  product.
    Time complexity : O(n^3)
    Space complexity : O(1)
    
    2. First sorting the given array. Now we have two cases to check. The first one is product of maximum, second maximum, and third maximum and the second is product of minimum, second minimmum and  maximum element. Whichever maximum is the answer. As we have sorted the array, the required elements can be accessed easily.
    Time complexity : O(nlogn)
    Space complexity : O(1)
    
    3. Similar to the second approach, we find the required elements but instead of sorting the array (which makes the time complexity O(nlogn)), we find those elements by traversing the array once and keeping track of the required elements.
    Time complexity : O(n)
    Space complexity : O(1)

---
+++ Array/max_product_triplet.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int MaxProduct(vector<int> &arr){
+    int n=arr.size();
+
+    int max1=INT_MIN;
+    int max2=INT_MIN;
+    int max3=INT_MIN;
+
+    int min1=INT_MAX;
+    int min2=INT_MAX;
+
+    for(int i=0;i<n;i++){
+        if(arr[i]>max1){
+            max3=max2;
+            max2=max1;
+            max1=arr[i];
+        }
+        else if(arr[i]<max1 && arr[i]>max2){
+            max3=max2;
+            max2=arr[i];
+        }
+        else if(arr[i]<max2 && arr[i]>max3){
+            max3=arr[i];
+        }
+
+        if(arr[i]<min1){
+            min2=min1;
+            min1=arr[i];
+        }
+        else if(arr[i]>min1 && arr[i]<min2){
+            min2=arr[i];
+        }
+    }
+    return max(max1*max2*max3 , min1*min2*max1);
+}
+
+int main(){
+    vector<int> arr={1,-4,3,-6,7,0};
+    cout<<MaxProduct(arr)<<" ";
+
+    return 0;
+//O(n), O(1)
+}
