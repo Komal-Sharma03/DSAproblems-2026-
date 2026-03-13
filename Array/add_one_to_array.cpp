ddbad33e9aca8854426fbc3a26c69d81eb48942b
KOMALSHARMA <komalsharmagcloud@gmail.com>
2026-03-13 23:42:37 +0530

    Add function to increment array representing a number
    Problem : Given a non-negative number represented as an array of digits. The task is to add 1 to the number.
    
    Approach : We will take care of the carry and the renewed digit to be stored in the array at every index. I found the way of doing so very informative and will use this way in other problems as well. The insert function is also very useful.
    
    Time complexity : O(n)
    Space complexity : O(1)

---
+++ Array/add_one_to_array.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void AddOneEnd(vector<int> &arr){
+    int n=arr.size();
+    int carry=1;
+    for(int i=n-1;i>=0;i--){
+        arr[i]+=carry;
+        carry=arr[i]/10;
+        arr[i]=arr[i]%10;
+    }
+    if(carry){
+        arr.insert(arr.begin(),carry);
+    }
+}
+
+int main(){
+    vector<int> arr={1,2,4};
+    AddOneEnd(arr);
+    for(int x:arr){
+        cout<<x;
+    }
+
+    return 0;
+}
