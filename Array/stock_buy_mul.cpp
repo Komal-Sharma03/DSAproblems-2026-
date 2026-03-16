2660611671d6b60a1260adf4a7c5ed4d366ae1bc
KOMALSHARMA <komalsharmagcloud@gmail.com>
2026-03-16 23:51:13 +0530

    Implement stock buy and sell - multiple transactions allowed algorithm
    Problem : Stock Buy and Sell - Multiple Transactions Allowed
    
    Approach : This approach involves the use of recursion. 
    
    Points to Remember : 
    1. How recursion was used in this kind of problem effortlessly. 
    2. Placement of ans=max(ans,curr)
    
    Time complexity : O(2^n)
    Space complexity : O(n)

---
+++ Array/stock_buy_mul.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int StockMul(vector<int> &arr,int start, int end){
+    int ans=0;
+    for(int i=start;i<end;i++){
+        for(int j=i+1;j<=end;j++){
+           if(arr[j]>arr[i]){
+              int curr=(arr[j]-arr[i])+StockMul(arr,start,i-1)+StockMul(arr,j+1,end);
+              ans=max(ans,curr);
+           }
+        }
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> arr={100,180,260,310,40,535,695};
+    cout<<StockMul(arr,0,arr.size()-1)<<endl;
+
+    return 0;
+
+    //O(2^n), O(n)
+}
