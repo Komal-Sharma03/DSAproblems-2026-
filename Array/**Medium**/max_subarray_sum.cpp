5a8de9b286dfdd21c987c402b8bbf9d797527962
KOMALSHARMA <komalsharmagcloud@gmail.com>
2026-03-23 23:36:49 +0530

    Maximum Subarray Sum (O(n^2))
    Problem : Given an integer array arr[], find the subarray (containing at least one element) which has the maximum possible sum,
    and return that sum.
    
    Approach : Here we use two for loops to find sum of each possible subarray and compare it with the maxSum, the ultimate maximum sum is then returned. 
    
    Time complexity : O(n^2)
    Space complexity : O(1)

---
+++ Array/**Medium**/max_subarray_sum.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int MaxSumSubarray(vector<int> &arr){
+    int maxSum=arr[0];
+    int n=arr.size();
+    for(int i=0;i<n;i++){
+        int currSum=0;
+        for(int j=i;j<n;j++){
+            currSum+=arr[j];
+        }
+        maxSum=max(maxSum,currSum);
+    }
+
+    return maxSum;
+}
+
+int main(){
+    vector<int> arr={2,3,-8,7,-1,2,3};
+    cout<<MaxSumSubarray(arr);
+
+    return 0;
+
+    //O(n^2), O(1)
+}
