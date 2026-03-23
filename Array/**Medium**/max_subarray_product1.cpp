553da7608e4d6f9c6f53eae3367e8eb496143f1a
KOMALSHARMA <komalsharmagcloud@gmail.com>
2026-03-24 02:09:45 +0530

    Maximum Product Subarray (O(n^2))
    Problem : Given an array arr[] consisting of positive, negative and zero values, find the maximum product that can be obtained from any contiguous subarray of arr[].
    Return that product.
    
    Approach : Here, We use two for loops to consider every possible subarray and find the maximum product.
    
    Time complexity : O(n)
    Space complexity : O(1)

---
+++ Array/**Medium**/max_subarray_product1.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int MaxProdSubarray(vector<int> &arr){
+    int n=arr.size();
+    int maxP=arr[0];
+    for(int i=0;i<n;i++){
+        int currP=1;
+        for(int j=i;j<n;j++){
+            currP*=arr[j];
+        
+            maxP=max(maxP,currP);
+        }
+    }
+    return maxP;
+}
+
+int main(){
+    vector<int> arr={-2,6,-3,-10,0,2};
+    cout<<MaxProdSubarray(arr);
+
+    return 0;
+}
