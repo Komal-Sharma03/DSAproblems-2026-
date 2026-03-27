d9fc8e69af6f9e361c880b8386d4595df81faacb
KOMALSHARMA <komalsharmagcloud@gmail.com>
2026-03-27 22:34:01 +0530

    Binary Search using Recursion
    Given array is already sorted in increasing order.
    
    Time Complexity : O(logn)
    Space Complexity : O(1)

---
+++ Search & sorting/binary_search_recursion.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int BinarySearch(vector<int> &arr,int low,int high,int target){
+       int mid=low+(high-low)/2;
+       while(left<=right){
+       if(arr[mid]==target){
+          return mid;
+       }
+       else if(arr[mid]<target){
+          return BinarySearch(arr,mid+1,high,target);
+       }
+       return BinarySearch(arr,low,mid-1,target);
+       }
+
+       return -1;
+}
+
+int main(){
+    vector<int> arr={2,3,4,10,40};
+    int target=10;
+    cout<<BinarySearch(arr,0,arr.size()-1,target);
+
+    return 0;
+}
