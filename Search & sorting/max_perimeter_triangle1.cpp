e2b12867f83cccee1a2ec7f6c9a03dd806433aed
KOMALSHARMA <komalsharmagcloud@gmail.com>
2026-03-31 17:16:18 +0530

    Maximum Perimeter Triangle (array)  (O(n^3), O(1))
    Problem : Given an array arr[] of positive integers. Find out maximum perimeter of the triangle from the array.
    Return -1, if it is not possible to construct a triangle.
    
    Approach : In this approach, we consider every possible combination of 3 sides given in the array.
    
    Key Take away : 
    1. Condition to form a triangle, all sides must follow.
    2. Use of j=i+1, k=j+1 produce combination not permutation which is good for this problem.
    
    Time complexity : O(n^3)
    Space complexity : O(1)

---
+++ Search & sorting/max_perimeter_triangle1.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int MaxPeri(vector<int> &arr){
+    int n=arr.size();
+    if(n<3) return -1;
+    int maxP=-1;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            for(int k=j+1;k<n;k++){
+                    if(arr[i]+arr[j]>arr[k] && arr[j]+arr[k]>arr[i] && arr[i]+arr[k]>arr[j]){
+                        maxP=max(maxP,arr[i]+arr[j]+arr[k]);
+                    }
+            
+            }
+        }
+    }
+
+    return maxP;
+}
+
+
+int main(){
+    vector<int> arr={6,1,6,5,8,4};
+    cout<<MaxPeri(arr);
+
+    return 0;
+
+    //O(n^3), O(1)
+}
