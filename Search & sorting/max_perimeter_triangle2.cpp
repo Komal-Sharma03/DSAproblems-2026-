c615726cc86815523759922c01ac3adc95f65e35
KOMALSHARMA <komalsharmagcloud@gmail.com>
2026-03-31 17:26:53 +0530

    Maximum Perimeter Triangle (array) (O(nlogn), O(1))
    Problem : Problem : Given an array arr[] of positive integers. Find out maximum perimeter of the triangle from the array.
    Return -1, if it is not possible to construct a triangle.
    
    Approach : Here, We sort the array and use this as an advantage to not check more than required side combinations.
    
    Key Take away :
    1. if arr[i]>arr[i-1]>arr[i-2], and arr[i]>arr[i-1]+arr[i-2], then no other arr[i-3], arr[i-4].... can come together such that their sum is more than arr[i]. So, we just have to check three consecutive sides.
    2. Here, I have sorted the array in ascending order, but we can also solve this by sorting the array in descending order by 
    sort(arr.begin(), arr.end(), greater<int>());
    
    Time complexity : O(nlogn)
    Space complexity : O(1)

---
+++ Search & sorting/max_perimeter_triangle2.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int MaxPeri(vector<int> &arr){
+    int n=arr.size();
+    if(n<3) return -1;
+    sort(arr.begin(),arr.end());
+    int maxP=-1;
+    for(int i=n-1;i>3;i--){
+        if(arr[i]<arr[i-1]+arr[i-2]){
+            maxP=max(maxP,arr[i]+arr[i-1]+arr[i-2]);
+        }
+    }
+    return maxP;
+}
+
+int main(){
+    vector<int> arr={7,55,20,1,4,33,12};
+    cout<<MaxPeri(arr);
+
+    return 0;
+
+    //O(nlogn), O(1)
+}
